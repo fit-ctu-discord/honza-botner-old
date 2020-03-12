eecaf6c30ba0d65575e1e223990e544c73274fdc
Jirka Vrba <jiri@vrba.dev>
2020-03-12 12:07:06 +0100

    Emoji listing with total/daily mode

--- src/main/java/dev/vrba/botner/database/entities/CountedEmoji.java
+++ src/main/java/dev/vrba/botner/database/entities/CountedEmoji.java
@@ -10,10 +10,10 @@ import java.util.Date;
 @DatabaseTable(tableName = "counted_emoji")
 public class CountedEmoji {
 
-    public CountedEmoji() {}
+    public CountedEmoji() {
+    }
 
-    public CountedEmoji(long id, long used, Date firstUsedAt)
-    {
+    public CountedEmoji(long id, long used, Date firstUsedAt) {
         this.id = id;
         this.times = used;
         this.firstUsedAt = firstUsedAt;
@@ -28,9 +28,12 @@ public class CountedEmoji {
     @DatabaseField(dataType = DataType.DATE)
     public Date firstUsedAt;
 
-    public double getUsagePerDay()
-    {
+    public double getUsagePerDay() {
         long daysInUsage = ChronoUnit.DAYS.between(new Date().toInstant(), this.firstUsedAt.toInstant()) + 1;
         return (double) this.times / daysInUsage;
+    }
+
+    public long getTotalUsage() {
+        return this.times;
     }
 }
--- src/main/java/dev/vrba/botner/discord/commands/emoji/ListEmojiCountingStatsCommand.java
+++ src/main/java/dev/vrba/botner/discord/commands/emoji/ListEmojiCountingStatsCommand.java
@@ -32,13 +32,19 @@ public class ListEmojiCountingStatsCommand extends ModCommand {
                 this.counter = new EmojiCounter(event.getApi());
             }
 
+            boolean total = false;
             List<CountedEmoji> results = this.counter.all();
-            Comparator<CountedEmoji> sortByUsage = Comparator.comparing(CountedEmoji::getUsagePerDay);
+            Comparator<CountedEmoji> comparator = Comparator.comparing(CountedEmoji::getUsagePerDay);
 
-            if (parameters.length > 0 && parameters[0].equals("reverse")) {
-                results.sort(sortByUsage);
+            if (Arrays.asList(parameters).contains("total")) {
+                total = true;
+                comparator = Comparator.comparing(CountedEmoji::getTotalUsage);
+            }
+
+            if (Arrays.asList(parameters).contains("reverse")) {
+                results.sort(comparator);
             } else {
-                results.sort(Collections.reverseOrder(sortByUsage));
+                results.sort(Collections.reverseOrder(comparator));
             }
 
             Optional<TextChannel> channel = event.getChannel().asTextChannel();
@@ -61,13 +67,14 @@ public class ListEmojiCountingStatsCommand extends ModCommand {
                 if (_wrapper.isPresent()) {
                     KnownCustomEmoji emoji = _wrapper.get();
 
+                    String label = total ? "×" : "×/day";
 
                     builder.append(emoji.getMentionTag())
                             .append("`")
-                            .append(StringUtils.leftPad(String.valueOf(result.getUsagePerDay()), 10, " "))
-                            .append("×/day`")
+                            .append(StringUtils.leftPad(String.valueOf(total ? result.getTotalUsage() : result.getUsagePerDay()), 10, " "))
+                            .append(label)
+                            .append("`")
                             .append(emojisAppended % 3 == 2 ? "\n" : "\t");
-
 
                     emojisAppended++;
                 }
